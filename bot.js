622421c5d18cd1577f83fd753a7ad1dd9e0069fb
backplateorbit <backplateorbit@gmail.com>
2020-09-28 16:33:04 +0100

    Implemented message crawler and normaliser.

--- bot.js
+++ bot.js
@@ -5,16 +5,86 @@ client.on("ready", () => {
     console.log("Logged in.")
 })
 
-client.on("message", message => {
-    if (message.content == "ping") {
-        message.reply("pong")
+client.on("message", async (message) => {
+    if (message.content == "ARCHIVE_COMMAND_PROTOTYPE") {
+        const server = await message.guild.fetch();
+        const serverMessages = await serverMessageExtractor(server)        
+        const formedMessages = serverMessages.map(
+            message => {
+                return {
+                    "username": message.author.username,
+                    "userId": message.author.id,
+                    "deleted": message.deleted,
+                    "timestamp": message.createdTimestamp,
+                    "id": message.id,
+                    "content": message.content,
+                    "channelId": message.channel.id,
+                    "channelName": message.channel.name,
+                    "embeds": message.embeds.map(
+                        embed => {
+                            return {
+                                "embedTitle": embed.title,
+                                "embedDesc": embed.description,
+                                "embedUrl": embed.url
+                            }
+                        }
+                    ),
+                    "attachments": message.attachments.map(
+                        attachment => {
+                            return {
+                                "attachmentId": attachment.id,
+                                "attachmentName": attachment.name,
+                                "size": attachment.size,
+                                "url": attachment.url,
+                            }
+                        }
+                    )
+                }
+            }
+        )
+        message.reply(`There are ${formedMessages.length} messages on the server.`)
     }
 })
 
-client.on("message", message => {
-    if (message.content == "ARCHIVE_COMMAND_PROTOTYPE") {
-        message.reply(JSON.stringify(client.guilds))
+const messageExtractor = async (channelObject) => {
+    let channelMessages = [];
+    let scrollId;
+
+    while (true) {
+        const options = {
+            limit: 100
+        }
+
+        if (scrollId) {
+            options["before"] = scrollId;
+        }
+
+        const scrollMessages = await channelObject.messages.fetch(options);
+        channelMessages.push(...scrollMessages.array());
+        scrollId = scrollMessages.last().id;
+
+        if (scrollMessages.size < 100) {
+            break
+        }
     }
-})
+
+    return channelMessages;
+}
+
+const serverMessageExtractor = async (serverObject) => {
+    const serverMessages = [];
+
+    const textChannels = serverObject.channels.cache.filter(
+        channel => channel.type === "text" && channel.name === "bot-testing"
+    )
+
+    for (channel of textChannels) {
+        const channelObject = await channel[1].fetch();
+        const channelMessages = await messageExtractor(channelObject);
+        serverMessages.push(...channelMessages);
+    }
+
+    return serverMessages;
+}
 
 client.login(process.env.BOT_TOKEN)

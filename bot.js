3d24e974d1583317a2019f9fb71cc27cd96c0918
backplateorbit <backplateorbit@gmail.com>
2020-09-28 12:53:27 +0100

    identifying guilds

--- bot.js
+++ bot.js
@@ -1,13 +1,19 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js")
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Logged in.')
+client.on("ready", () => {
+    console.log("Logged in.")
 })
 
-client.on('message', message => {
-    if (message.content == 'ping') {
-        message.reply('pong')
+client.on("message", message => {
+    if (message.content == "ping") {
+        message.reply("pong")
+    }
+})
+
+client.on("message", message => {
+    if (message.content == "ARCHIVE_COMMAND_PROTOTYPE") {
+        message.reply(JSON.stringify(client.guilds))
     }
 })
 
